1f9a1f05c342b5d36a54ae59cdf0c19b819df887
Karim <karim.jedda@gmail.com>
2016-11-26 21:09:32 +0100

    Added postgres database and updated versions for Python deps

--- djreact/settings.py
+++ djreact/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'djreact.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_react',
+        'USER': 'postgres',
+        'PASSWORD': '1internet',
+        'HOST': 'localhost',
+        'PORT': ''
+    },
 }
 
 
